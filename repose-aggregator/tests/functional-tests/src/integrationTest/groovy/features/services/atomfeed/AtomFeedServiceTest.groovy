d39351ec8e720a595baeff3e91ca0a980c440530
Damien Johnson <damien.johnson@rackspace.com>
2018-05-23 14:14:17 -0500

    Hijacking the AtomFeedServiceTest to test processing of a multi-page Feed that gets updated

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/services/atomfeed/AtomFeedServiceTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/services/atomfeed/AtomFeedServiceTest.groovy
@@ -20,14 +20,19 @@
 package features.services.atomfeed
 
 import features.filters.keystonev2.AtomFeedResponseSimulator
+import groovy.xml.MarkupBuilder
 import org.junit.experimental.categories.Category
 import org.openrepose.framework.test.ReposeValveTest
+import org.rackspace.deproxy.Request
+import org.rackspace.deproxy.Response
 import scaffold.category.Slow
 import org.openrepose.framework.test.mocks.MockIdentityV2Service
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.Endpoint
 
 import java.util.concurrent.TimeUnit
+
+import static javax.servlet.http.HttpServletResponse.SC_OK
 
 @Category(Slow.class)
 class AtomFeedServiceTest extends ReposeValveTest {
@@ -63,65 +68,199 @@ class AtomFeedServiceTest extends ReposeValveTest {
         repose?.stop()
     }
 
-    def "when an atom feed entry is received, it is passed to the filter"() {
-        given: "there is an atom feed entry available for consumption"
-        def atomFeedEntry = fakeAtomFeed.atomEntryForTokenInvalidation(id: 'urn:uuid:101')
-        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntry(atomFeedEntry)
+//    def "when an atom feed entry is received, it is passed to the filter"() {
+//        given: "there is an atom feed entry available for consumption"
+//        def atomFeedEntry = fakeAtomFeed.atomEntryForTokenInvalidation(id: 'urn:uuid:101')
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntry(atomFeedEntry)
+//
+//        when: "we wait for the Keystone V2 filter to read the feed"
+//        reposeLogSearch.awaitByString("</atom:entry>", 1, 11, TimeUnit.SECONDS)
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handler
+//
+//        then: "the Keystone V2 filter logs receiving the atom feed entry"
+//        AtomFeedResponseSimulator.buildXmlToString(atomFeedEntry).eachLine { line ->
+//            assert reposeLogSearch.searchByString(line.trim()).size() == 1
+//            true
+//        }
+//    }
+//
+//    def "when multiple atom feed entries are received, they are passed in-order to the filter"() {
+//        given: "there is a list of atom feed entries available for consumption"
+//        List<String> ids = (201..210).collect {it as String}
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
+//                ids.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
+//
+//        when: "we wait for the Keystone V2 filter to read the feed"
+//        reposeLogSearch.awaitByString("</atom:entry>", ids.size(), 11, TimeUnit.SECONDS)
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handler
+//
+//        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
+//        def logLines = reposeLogSearch.searchByString("<atom:id>.*</atom:id>")
+//        logLines.size() == ids.size()
+//        logLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == ids
+//    }
+//
+//    // todo: this test does not actually create a multi-page feed, but rather, replaces the feed at the base URI
+//    def "when multiple pages of atom feed entries are received, they are all processed by the filter"() {
+//        given: "there is a list of atom feed entries available for consumption"
+//        List<String> ids = (301..325).collect {it as String}
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
+//                ids.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
+//
+//        when: "we wait for the Keystone V2 filter to read the feed"
+//        reposeLogSearch.awaitByString("</atom:entry>", ids.size(), 11, TimeUnit.SECONDS)
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handler
+//
+//        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
+//        def logLines = reposeLogSearch.searchByString("<atom:id>.*</atom:id>")
+//        logLines.size() == ids.size()
+//        logLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == ids
+//
+//        when: "there are more entries on the next page"
+//        def moreIds = (401..425).collect {it as String}
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
+//                moreIds.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
+//
+//        and: "we wait for the Keystone V2 filter to read the feed"
+//        reposeLogSearch.awaitByString("</atom:entry>", ids.size() + moreIds.size(), 11, TimeUnit.SECONDS)
+//        atomEndpoint.defaultHandler = fakeAtomFeed.handler
+//
+//        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
+//        def moreLogLines = reposeLogSearch.searchByString("<atom:id>.*4\\d{2}</atom:id>")
+//        moreLogLines.size() == moreIds.size()
+//        moreLogLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == moreIds
+//    }
 
-        when: "we wait for the Keystone V2 filter to read the feed"
-        reposeLogSearch.awaitByString("</atom:entry>", 1, 11, TimeUnit.SECONDS)
-        atomEndpoint.defaultHandler = fakeAtomFeed.handler
+    def "when new events are posted to the feed, they are all processed by the filter"() {
+        given:
+        def params = fakeAtomFeed.defaultParams + fakeAtomFeed.DEFAULT_FEED_PARAMS
+        def initialFeedPg1 =
+            fakeAtomFeed.buildXmlToString { MarkupBuilder xmlBuilder ->
+                xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
 
-        then: "the Keystone V2 filter logs receiving the atom feed entry"
-        AtomFeedResponseSimulator.buildXmlToString(atomFeedEntry).eachLine { line ->
-            assert reposeLogSearch.searchByString(line.trim()).size() == 1
-            true
+                xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+                    'id'(params.id)
+                    'title'(type: "text", "feed")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:1&amp;limit=1&amp;search=&amp;direction=backward", rel: "next")
+                    'updated'(params.time)
+                    fakeAtomFeed.atomEntryForUserUpdate(id: "urn:uuid:2")(xmlBuilder)
+                }
+
+                xmlBuilder
+            }
+
+        def initialFeedPg2 =
+            fakeAtomFeed.buildXmlToString { MarkupBuilder xmlBuilder ->
+                xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
+
+                xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+                    'id'(params.id)
+                    'title'(type: "text", "feed")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=last&amp;limit=1&amp;search=&amp;direction=backward", rel: "last")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:2&amp;limit=1&amp;search=&amp;direction=forward", rel: "previous")
+                    'updated'(params.time)
+                    fakeAtomFeed.atomEntryForUserUpdate(id: "urn:uuid:1")(xmlBuilder)
+                }
+
+                xmlBuilder
+            }
+
+        def updatedFeedPg1 =
+            fakeAtomFeed.buildXmlToString { MarkupBuilder xmlBuilder ->
+                xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
+
+                xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+                    'id'(params.id)
+                    'title'(type: "text", "feed")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:2&amp;limit=1&amp;search=&amp;direction=backward", rel: "next")
+                    'updated'(params.time)
+                    fakeAtomFeed.atomEntryForUserUpdate(id: "urn:uuid:3")(xmlBuilder)
+                }
+
+                xmlBuilder
+            }
+
+        def updatedFeedPg2 =
+            fakeAtomFeed.buildXmlToString { MarkupBuilder xmlBuilder ->
+                xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
+
+                xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+                    'id'(params.id)
+                    'title'(type: "text", "feed")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:1&amp;limit=1&amp;search=&amp;direction=backward", rel: "next")
+                    'updated'(params.time)
+                    fakeAtomFeed.atomEntryForUserUpdate(id: "urn:uuid:2")(xmlBuilder)
+                }
+
+                xmlBuilder
+            }
+
+        def updatedFeedPg3 =
+            fakeAtomFeed.buildXmlToString { MarkupBuilder xmlBuilder ->
+                xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
+
+                xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+                    'id'(params.id)
+                    'title'(type: "text", "feed")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=last&amp;limit=1&amp;search=&amp;direction=backward", rel: "last")
+                    'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:2&amp;limit=1&amp;search=&amp;direction=forward", rel: "previous")
+                    'updated'(params.time)
+                    fakeAtomFeed.atomEntryForUserUpdate(id: "urn:uuid:1")(xmlBuilder)
+                }
+
+                xmlBuilder
+            }
+
+        when:
+        atomEndpoint.defaultHandler = { Request request ->
+            if(request.path.contains("marker=urn:uuid:1")) {
+                new Response(SC_OK,
+                    null,
+                    fakeAtomFeed.headers,
+                    initialFeedPg2)
+            } else {
+                new Response(SC_OK,
+                    null,
+                    fakeAtomFeed.headers,
+                    initialFeedPg1)
+            }
         }
-    }
+        reposeLogSearch.awaitByString("</atom:entry>", 2, 11, TimeUnit.SECONDS)
 
-    def "when multiple atom feed entries are received, they are passed in-order to the filter"() {
-        given: "there is a list of atom feed entries available for consumption"
-        List<String> ids = (201..210).collect {it as String}
-        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
-                ids.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
+        and:
+        atomEndpoint.defaultHandler = { Request request ->
+            if(request.path.contains("marker=urn:uuid:1")) {
+                new Response(SC_OK,
+                    null,
+                    fakeAtomFeed.headers,
+                    updatedFeedPg3)
+            } else if(request.path.contains("marker=urn:uuid:2")) {
+                new Response(SC_OK,
+                    null,
+                    fakeAtomFeed.headers,
+                    updatedFeedPg2)
+            } else {
+                new Response(SC_OK,
+                    null,
+                    fakeAtomFeed.headers,
+                    updatedFeedPg1)
+            }
+        }
+        reposeLogSearch.awaitByString("</atom:entry>", 3, 11, TimeUnit.SECONDS)
 
-        when: "we wait for the Keystone V2 filter to read the feed"
-        reposeLogSearch.awaitByString("</atom:entry>", ids.size(), 11, TimeUnit.SECONDS)
-        atomEndpoint.defaultHandler = fakeAtomFeed.handler
-
-        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
-        def logLines = reposeLogSearch.searchByString("<atom:id>.*</atom:id>")
-        logLines.size() == ids.size()
-        logLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == ids
-    }
-
-    def "when multiple pages of atom feed entries are received, they are all processed by the filter"() {
-        given: "there is a list of atom feed entries available for consumption"
-        List<String> ids = (301..325).collect {it as String}
-        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
-                ids.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
-
-        when: "we wait for the Keystone V2 filter to read the feed"
-        reposeLogSearch.awaitByString("</atom:entry>", ids.size(), 11, TimeUnit.SECONDS)
-        atomEndpoint.defaultHandler = fakeAtomFeed.handler
-
-        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
-        def logLines = reposeLogSearch.searchByString("<atom:id>.*</atom:id>")
-        logLines.size() == ids.size()
-        logLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == ids
-
-        when: "there are more entries on the next page"
-        def moreIds = (401..425).collect {it as String}
-        atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries(
-                moreIds.collect { fakeAtomFeed.atomEntryForTokenInvalidation(id: "urn:uuid:$it") })
-
-        and: "we wait for the Keystone V2 filter to read the feed"
-        reposeLogSearch.awaitByString("</atom:entry>", ids.size() + moreIds.size(), 11, TimeUnit.SECONDS)
-        atomEndpoint.defaultHandler = fakeAtomFeed.handler
-
-        then: "the Keystone V2 filter logs receiving the atom feed entries in order"
-        def moreLogLines = reposeLogSearch.searchByString("<atom:id>.*4\\d{2}</atom:id>")
-        moreLogLines.size() == moreIds.size()
-        moreLogLines.collect { (it =~ /\s*<atom:id>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == moreIds
+        then:
+        def logLines = reposeLogSearch.searchByString(">.*\\d+</atom:id>")
+        logLines.size() == 3
+        logLines.collect { (it =~ /\s*>urn:uuid:(\d+)<\/atom:id>.*/)[0][1] } == [2, 1, 3]
     }
 }
